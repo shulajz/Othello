4066fc269b03d01d21acfdda928d86647f53031e
Shulamit Ajzner <shulajz@gmail.com>
2017-11-23 18:29:19 +0200

    getting ready for tests

--- gtest_src/main_test.cpp
+++ gtest_src/main_test.cpp
@@ -2,3 +2,14 @@
 // Created by shulamit on 23/11/17.
 //
 
+#include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using namespace testing;
+
+int main(int argc, char *argv[]) {
+    InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
+}
