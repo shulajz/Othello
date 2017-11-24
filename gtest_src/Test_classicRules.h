6d8fc7727657a7629ea62d9b88d85c166b3b9364
Shulamit Ajzner <shulajz@gmail.com>
2017-11-24 15:26:42 +0200

    getting ready for tests

---
+++ gtest_src/Test_classicRules.h
@@ -0,0 +1,14 @@
+//
+// Created by shulamit on 24/11/17.
+//
+
+#ifndef OTHELLO_TEST_CLASSICRULES_H
+#define OTHELLO_TEST_CLASSICRULES_H
+
+
+class Test_classicRules {
+
+};
+
+
+#endif //OTHELLO_TEST_CLASSICRULES_H
