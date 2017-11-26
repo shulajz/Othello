a265439bf62294435e387f8bf5ad9459cc876819
Or Shaashua <orshaashua@gmail.com>
2017-11-26 05:13:45 -0800

    test 2 functions of doOneTurn in Test_AIPlayer

--- gtest_src/Test_AIPlayer.cpp
+++ gtest_src/Test_AIPlayer.cpp
@@ -41,10 +41,6 @@ TEST_F(Test_AIPlayer, doOneTurn2){
     delete(realPlayer);
 }
 
-//calcTheMaxScoreForOneTurn(GameRules *gameRules, Board &tempBoard,
-//        vector<Coordinate> &validCoordinates, int &maxLocal)
-
-
 TEST_F(Test_AIPlayer, calcTheMaxScoreForOneTurn){
     vector<Coordinate> validCoordinates;
     ClassicRules classicRules;
@@ -52,13 +48,13 @@ TEST_F(Test_AIPlayer, calcTheMaxScoreForOneTurn){
     int maxLocal = 0;
     Player *aiPlayer1 = new AIPlayer(White);
     //flip the blacks first
-    Coordinate testingCoordinate1 = {0};
+    Coordinate testingCoordinate1={0};
     testingCoordinate1.row = 3;
     testingCoordinate1.col = 4;
     board_8x8->updateValue(testingCoordinate1, Black);
     classicRules.flipTokens(testingCoordinate1, *board_8x8, realPlayer);
     // one turn of the white
-    Coordinate testingCoordinate = {0};
+    Coordinate testingCoordinate={0};
     testingCoordinate.row = 3;
     testingCoordinate.col = 3;
 
@@ -66,6 +62,6 @@ TEST_F(Test_AIPlayer, calcTheMaxScoreForOneTurn){
     classicRules.flipTokens(testingCoordinate, *board_8x8, aiPlayer1);
 
     aiPlayer1->calcTheMaxScoreForOneTurn(&classicRules, *board_8x8, validCoordinates, maxLocal);
-    ASSERT_EQ(maxLocal, 3) << "calcTheMaxScoreForOneTurn test failed";
+    ASSERT_EQ(maxLocal, 5) << "calcTheMaxScoreForOneTurn test failed";
     delete(realPlayer);
 }
