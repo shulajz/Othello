e836128f95053481de9c8cdf62d2889279ac3c4e
Or Shaashua <orshaashua@gmail.com>
2017-12-11 10:42:27 -0800

    adding makefiles to server and client and adding to exe server.out client.out and  adding setting files and log.txt

--- ex4/src/client/src/Board.cpp
+++ ex4/src/client/src/Board.cpp
@@ -33,12 +33,12 @@ Board::Board(int dimensions, TokenFactory &pTokenFactory,
     }
 
     //one teo three
-//
-//    for(int i = 0; i < NUM_OF_INITIAL_CELLS; i++) {
-//        //set values to the initial cells
-//        boardArr[initialCells[i].coordinate.row][initialCells[i].coordinate.col].
-//                setValue(initialCells[i].tv);
-//    }
+
+    for(int i = 0; i < NUM_OF_INITIAL_CELLS; i++) {
+        //set values to the initial cells
+        boardArr[initialCells[i].coordinate.row][initialCells[i].coordinate.col].
+                setValue(initialCells[i].tv);
+    }
 
 //    for(int i = 1; i < 5; i++) {
 //        for (int j = 1; j < 5; j++) {
@@ -65,15 +65,15 @@ Board::Board(int dimensions, TokenFactory &pTokenFactory,
 //    boardArr[1][5].setValue(Empty);
 //    boardArr[2][4].setValue(White);
 
-    for(int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            boardArr[i][j].setValue(Black);
-        }
-    }
-        boardArr[4][1].setValue(Black);
-        boardArr[4][4].setValue(Empty);
-        boardArr[4][3].setValue(Empty);
-        boardArr[3][3].setValue(White);
+//    for(int i = 0; i < 5; i++) {
+//        for (int j = 0; j < 5; j++) {
+//            boardArr[i][j].setValue(Black);
+//        }
+//    }
+//        boardArr[4][1].setValue(Black);
+//        boardArr[4][4].setValue(Empty);
+//        boardArr[4][3].setValue(Empty);
+//        boardArr[3][3].setValue(White);
 
 //        boardArr[3][1].setValue(White);
 //        boardArr[3][3].setValue(Empty);
