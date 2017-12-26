799fd96188fbda0c50d18b6a780a2a1809db1e1e
Shulamit Ajzner <shulajz@gmail.com>
2017-12-26 16:59:36 +0200

    editing

--- ex4/src/server/HandleClient.cpp
+++ ex4/src/server/HandleClient.cpp
@@ -35,16 +35,16 @@ void* HandleClient :: gateFunction(void* element) {
 
 void HandleClient::handleClient(void* element) {
     ClientData* data = (ClientData*)element;
-    bool isContinue = true;
-    while(isContinue) {
-
+//    bool isContinue = true;
+//    while(isContinue) {
         string command;
         string args;
+        cout << "the socket now is = " << data->clientSocket << endl;
         readCommand(data->clientSocket, command, args);
         pthread_mutex_lock(&this->handle_client_locker);
-        isContinue = commandsManager.executeCommand(command, args, data);
+        commandsManager.executeCommand(command, args, data);
         pthread_mutex_unlock(&this->handle_client_locker);
-    }
+//    }
 
 }
 
